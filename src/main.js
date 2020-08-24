403906ec850bbe91e5748034d5758a67369ccecb
villasurlamer <loic.dekester@gmail.com>
2020-08-23 19:55:26 -0700

    added bootstrap vue

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
 
 import { CHECK_AUTH } from "./store/actions.type";
 import ApiService from "./common/service/api";
@@ -14,6 +17,9 @@ ApiService.init();
 router.beforeEach((to, from, next) =>
   Promise.all([store.dispatch(CHECK_AUTH)]).then(next)
 );
+
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
 
 new Vue({
   router,
