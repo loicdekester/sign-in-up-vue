e93f292889e4cf217cf6862e3718d619dc581a13
villasurlamer <loic.dekester@gmail.com>
2020-08-30 16:54:06 -0700

    remove old code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,35 +3,17 @@
     <div class="container">
       <h1 class="logo-font">Home</h1>
       <p>This is the Home page</p>
-      <button class="btn btn-lg btn-primary pull-xs-right" @click="getProfile">
-      Get Profile
-      </button>
-    </div>
-    <div v-if="user">
-      {{user.firstName}} {{user.lastName}}
     </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
-import {FETCH_USER} from "../store/actions.type";
-
 export default {
   name: "HomeVue",
   components: {
   },
   mounted() {
   },
-  computed: {
-    ...mapGetters(["user"]),
-  },
-  methods:{
-    getProfile() {
-      this.$store
-        .dispatch(FETCH_USER);
-    }
-  }
 };
 </script>
 <style scoped>
