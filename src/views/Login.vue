be76e319c860cb07179240ef1fd065d080530e91
villasurlamer <loic.dekester@gmail.com>
2020-08-29 17:40:46 -0700

    style changes to the login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -13,30 +13,45 @@
             <li v-for="(v, k) in errors" :key="k">{{ k }} {{ v | error }}</li>
           </ul>
           <form @submit.prevent="onSubmit(email, password)">
-            <fieldset class="form-group">
-              <input
-                class="form-control form-control-lg"
-                type="text"
-                v-model="email"
-                placeholder="Email"
-                autocomplete="username"
-              />
-            </fieldset>
-            <fieldset class="form-group">
-              <input
-                class="form-control form-control-lg"
-                type="password"
-                v-model="password"
-                placeholder="Password"
-                autocomplete="current-password"
-              />
-            </fieldset>
+            <div class="row justify-content-center">
+              <fieldset class="form-group col-md-7">
+                <input
+                  class="form-control form-control-lg"
+                  type="text"
+                  v-model="email"
+                  placeholder="Email"
+                  autocomplete="username"
+                />
+              </fieldset>
+            </div>
+            <div class="row justify-content-center">
+              <fieldset class="form-group col-md-7">
+                <input
+                  class="form-control form-control-lg"
+                  type="password"
+                  v-model="password"
+                  placeholder="Password"
+                  autocomplete="current-password"
+                />
+              </fieldset>
+            </div>
             <button class="btn btn-lg btn-primary pull-xs-right">
-              Sign in
+              Login
             </button>
           </form>
-          <a href="http://localhost:3000/api/auth/google">Sign in with Google</a>
-          <a href="http://localhost:3000/api/auth/facebook">Login with Facebook</a>
+          <div class="other-login">
+            <div class="circle-container">
+              <p class="circled-text">Or</p>
+            </div>
+            <div class="link-container">
+              <div class="google">
+                <a class="social-link" href="http://localhost:3000/api/auth/google"><i class="google-logo"></i>Login with Google</a>
+              </div>
+              <div class="fb">
+                <a class="social-link" href="http://localhost:3000/api/auth/facebook"><i class="fb-logo"></i>Login with Facebook</a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -73,5 +88,80 @@ export default {
 <style scoped>
 .login-page {
   padding: 3rem 0;
+  text-align: center;
+}
+.fb {
+  background-color: #3b5998;
+  color: white;
+  padding: 12px;
+  border: none;
+  border-radius: 4px;
+  margin: 5px 0;
+  opacity: 0.85;
+  display: inline-block;
+}
+
+.fb-logo {
+  background-image: url(../../public/icons/facebook-brands.svg);
+  background-repeat: no-repeat;
+  width: 1.2em;
+  height: 1.2em;
+  display: inline-block;
+  filter: invert(100%);
+  margin: 0px 5px;
+}
+
+.google {
+  background-color: #dd4b39;
+  color: white;
+  padding: 12px;
+  border: none;
+  border-radius: 4px;
+  margin: 5px 0;
+  opacity: 0.85;
+  display: inline-block;
+}
+
+.google-logo {
+  background-image: url(../../public/icons/google-brands.svg);
+  background-repeat: no-repeat;
+  width: 1.2em;
+  height: 1.2em;
+  display: inline-block;
+  filter: invert(100%);
+  margin-right: 10px;
+}
+
+.social-link {
+  text-decoration: none;
+  color: white;
+  font-size: large;
+  font-weight: 500;
+  vertical-align: middle;
+}
+
+.link-container {
+  display: flex;
+  flex-direction: column;
+  width: fit-content;
+}
+
+.circled-text {
+  border: 1px solid #ccc;
+  border-radius: 50%;
+  padding: 7px 1px;
+  width: 40px;
+  height: 40px;
+  margin-bottom: 0px;
+}
+
+.circle-container {
+  margin: auto;
+  display: inline-block;
+  margin-top: 10px;
+}
+
+.other-login {
+  display: inline-block;
 }
 </style>
