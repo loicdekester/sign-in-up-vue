596fff2411223fe7ba50f060c549dd8fa115e421
villasurlamer <loic.dekester@gmail.com>
2020-08-30 17:51:57 -0700

    config to resolve img path in bootsrap components

---
+++ vue.config.js
@@ -0,0 +1,29 @@
+/* eslint-disable */
+//config to resolve img path in bootsrapvue components
+module.exports = {
+  chainWebpack: config => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .loader('vue-loader')
+      .tap(options => {
+        return {
+          transformAssetUrls: {
+            video: ['src', 'poster'],
+            source: 'src',
+            img: 'src',
+            image: 'xlink:href',
+            'b-avatar': 'src',
+            'b-img': 'src',
+            'b-img-lazy': ['src', 'blank-src'],
+            'b-card': 'img-src',
+            'b-card-img': 'src',
+            'b-card-img-lazy': ['src', 'blank-src'],
+            'b-carousel-slide': 'img-src',
+            'b-embed': 'src'
+          }
+        }
+      });
+  }
+}
+
